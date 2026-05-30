a17fd4deedf0977ee10cdf998d40b7d078e0afd9
Amira El Hafidi <amira.el.hafidi@hva.nl>
2026-05-30 23:46:42 +0200

    fix: docstring toegevoegd aan unit test

--- tests/test_code_generator.py
+++ tests/test_code_generator.py
@@ -2,9 +2,10 @@ from app.blockly.code_generator import xml_to_robot
 
 
 def test_open_browser_blok_vertaalt_correct():
-    # Simuleert een Blockly editor met één blok: Open Browser
-    # Dit is dezelfde XML die de editor verstuurt als een gebruiker een blok plaatst
+    """
+    Simuleert een Blockly editor met één blok: Open Browser.
+    Controleert of de vertaling de juiste Robot Framework keyword bevat.
+    """
     xml = '<xml><block type="open_browser"><field name="URL">https://www.hva.nl</field></block></xml>'
     _, robot_file = xml_to_robot(xml)
-    # Controleert of de vertaling de juiste Robot Framework keyword bevat
     assert "Open Browser    https://www.hva.nl" in robot_file
