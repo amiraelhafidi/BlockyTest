fb3a3abcfac0b59fc53f7119ba179f194a480a77
Ryan James Tiangco <tiangcr@ad.hva.nl>
2026-05-30 17:51:38 +0200

    in html de blok toegevoegd en ook bij de block map in code gen py

--- app/blockly/code_generator.py
+++ app/blockly/code_generator.py
@@ -11,6 +11,7 @@ BLOCK_MAP = {
     "click_element": ("Click Element", ["ELEMENT"]),
     "wait_for_element": ("Wait Until Element Is Visible", ["ELEMENT", "TIMEOUT"]),
     "assert_title": ("Title Should Be", ["TITLE"]),
+    "capture_screenshot": ("Capture Page Screenshot", []),
     "close_browser": ("Close Browser", []),
 }
 
