9e9f79b3a4c02be9a18fccc53feee27a59385045
Amira El Hafidi <amira.el.hafidi@hva.nl>
2026-05-30 23:44:59 +0200

    feat: unit test uitgelegd met comments

--- tests/test_code_generator.py
+++ tests/test_code_generator.py
@@ -1,7 +1,10 @@
 from app.blockly.code_generator import xml_to_robot
 
 
-def test_open_browser_genereert_juiste_code():
+def test_open_browser_blok_vertaalt_correct():
+    # Simuleert een Blockly editor met één blok: Open Browser
+    # Dit is dezelfde XML die de editor verstuurt als een gebruiker een blok plaatst
     xml = '<xml><block type="open_browser"><field name="URL">https://www.hva.nl</field></block></xml>'
     _, robot_file = xml_to_robot(xml)
+    # Controleert of de vertaling de juiste Robot Framework keyword bevat
     assert "Open Browser    https://www.hva.nl" in robot_file
